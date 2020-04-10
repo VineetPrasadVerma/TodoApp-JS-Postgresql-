74c1b18e3fd19735379b96333b922af008811ecf
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-04-11 00:16:35 +0530

    Done with CRUD operations on lists

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const db = require('./queries')
 const app = express()
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
 
 const port = 3000
 
@@ -8,6 +16,10 @@ app.get('/', (req, res) => {
   res.send('I am homepage')
 })
 
-app.get('/getAllLists', db.getAllLists)
+app.get('/getAllLists', db.queriesObject.getAllLists)
+app.get('/getListById/:id', db.queriesObject.getListById)
+app.post('/createList', db.queriesObject.createList)
+app.put('/getListById/:id', db.queriesObject.updateList)
+app.delete('/getListById/:id', db.queriesObject.deleteList )
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
--- queries.js
+++ queries.js
@@ -9,14 +9,61 @@ const pool = new Pool({
   port: process.env.DB_PORT
 })
 
-const getAllLists = async (req, res) => {
+const queriesObject = {}
+
+queriesObject.getAllLists = async (req, res) => {
   try {
-    const results = await pool.query('SELECT * from lists')
-    res.status(200).json(results.rows)
+    const result = await pool.query('SELECT * from lists')
+    res.status(200).json(result.rows)
   } catch (e) {
     // console.log(e)
     res.status(500).json(e)
   }
 }
 
-module.exports = { getAllLists }
+queriesObject.getListById = async (req, res) => {
+  try {
+    const listId = Number(req.params.id)
+    const result = await pool.query('SELECT * FROM lists WHERE list_id =  $1', [listId])
+    res.status(200).json(result.rows)
+  } catch (e) {
+    // console.log(e)
+    res.status(500).json(e)
+  }
+}
+
+queriesObject.createList = async (req, res) => {
+  try {
+    const listName = req.body.listName
+    const result = await pool.query('INSERT INTO LISTS (list_name) VALUES ($1) RETURNING list_id', [listName])
+    // console.log(result)
+    res.status(201).send(`List added with Id ${result.rows[0].list_id} `)
+  } catch (e) {
+    res.status(500).json(e)
+  }
+}
+
+queriesObject.updateList = async (req, res) => {
+  try {
+    const listId = Number(req.params.id)
+    const listName = req.body.listName
+    const result = await pool.query('UPDATE LISTS SET list_name = $1 WHERE list_id = $2', [listName, listId])
+    // console.log(result)
+    res.status(200).send(`List modified with ID: ${listId}`)
+  } catch (e) {
+    res.status(500).json(e)
+  }
+}
+
+queriesObject.deleteList = async (req, res) => {
+  try {
+    const listId = Number(req.params.id)
+    const result = await pool.query('DELETE FROM lists WHERE list_id =  $1', [listId])
+    res.status(200).send(`User deleted with ID: ${listId}`)
+  } catch (e) {
+    // console.log(e)
+    res.status(500).json(e)
+  }
+}
+
+module.exports = { queriesObject }
