4170c304e168b53d7dd9cef8df9c1435dc07037f
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-04-15 01:59:36 +0530

    Edit list completed and now the whole DOM is not rendering on adding or update of list

--- public/js/script.js
+++ public/js/script.js
@@ -8,11 +8,10 @@ const showTaskContainer = document.querySelector('#show-tasks-container')
 // const selectedList = []
 
 const baseURL = '/lists'
-let lists = []
+// let lists = []
 
 const fetchDB = async (reqObj) => {
   try {
-    console.log(reqObj.url)
     const res = await window.fetch(reqObj.url, reqObj.init)
     const data = await res.json()
 
@@ -40,32 +39,87 @@ const createElement = (type, props, ...children) => {
   return dom
 }
 
-const renderLists = lists => {
-  lists.forEach(list => {
-    const divElement = document.createElement('div')
-    const span = document.createElement('span')
-    span.textContent = list.list_name
-    span.id = 'list-item'
-    divElement.appendChild(span)
+const renderLists = list => {
+  // lists.forEach(list => {
+  //   const divElement = document.createElement('div')
+  //   const span = document.createElement('span')
+  //   span.textContent = list.list_name
+  //   span.id = 'list-item'
+  //   divElement.appendChild(span)
 
-    const span1 = document.createElement('span')
-    const span2 = document.createElement('span')
-    span1.innerHTML = '<i style="float:right; padding-right:10px" class="fa fa-pencil-square-o" aria-hidden="true"></i>'
-    span2.innerHTML = '<i style="float:right" class="fa fa-trash" aria-hidden="true"></i>'
-    divElement.appendChild(span2)
-    divElement.appendChild(span1)
+  //   const span1 = document.createElement('span')
+  //   const span2 = document.createElement('span')
+  //   span1.innerHTML = '<i style="float:right; padding-right:10px" class="fa fa-pencil-square-o" aria-hidden="true"></i>'
+  //   span2.innerHTML = '<i style="float:right" class="fa fa-trash" aria-hidden="true"></i>'
+  //   divElement.appendChild(span2)
+  //   divElement.appendChild(span1)
 
-    // console.log(span1.previousElementSibling)
-    span.setAttribute('onclick', 'renderSelectedListTasksOnClick(event)')
-    span1.setAttribute('onclick', 'editSelectedListOnClick(event)')
-    span2.setAttribute('onclick', 'deleteSelectedListOnClick(event)')
+  //   // console.log(span1.previousElementSibling)
+  //   span.setAttribute('onclick', 'renderSelectedListTasksOnClick(event)')
+  //   span1.setAttribute('onclick', 'editSelectedListOnClick(event)')
+  //   span2.setAttribute('onclick', 'deleteSelectedListOnClick(event)')
 
-    divElement.id = list.list_id
-    divElement.style.padding = '5px'
-    showListContainer.appendChild(divElement)
-  })
+  //   divElement.id = list.list_id
+  //   divElement.style.padding = '5px'
+  //   showListContainer.appendChild(divElement)
+  // })
+
+  const divList = createElement(
+    'div',
+    { id: list.list_id },
+    createElement('span', { id: 'list-item' }, list.list_name),
+    createElement('i', { className: 'fa fa-trash', ariahidden: 'true' }),
+    createElement('i', { className: 'fa fa-pencil-square-o', ariahidden: 'true', onclick: editList })
+
+  )
+
+  showListContainer.appendChild(divList)
 }
 
+const editList = event => {
+  const parentDiv = event.target.parentNode
+  parentDiv.childNodes[1].classList.add('hide')
+  parentDiv.childNodes[2].classList.add('hide')
+  const inputElement = createElement('input', { type: 'text', focus: true, value: parentDiv.firstChild.textContent })
+  // const input = document.createElement('input')
+  // input.type = 'text'
+  // input.value = parentDiv.firstChild.textContent
+  parentDiv.replaceChild(inputElement, parentDiv.firstChild)
+  // input.focus()
+  inputElement.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      // console.log(parentDiv.id)
+      // console.log(this.value)
+
+      const reqObj = {
+        url: baseURL + `/${event.target.parentNode.id}`,
+        init: {
+          method: 'PUT',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify({ listName: event.target.value })
+        }
+      }
+
+      if (event.target.value === '') {
+        inputElement.placeholder = 'Can\'t set empty name'
+        return
+      }
+
+      updateList(reqObj)
+
+      // console.log(event.target.parentNode.parentNode)
+      // reset(event.target.parentNode.parentNode)
+      // renderLists(lists)
+      // const span = document.createElement('span')
+      // span.textContent = this.value
+      const spanElement = createElement('span', { onclick: renderTask }, this.value)
+      parentDiv.replaceChild(spanElement, parentDiv.firstChild)
+      // span.setAttribute('onclick', 'renderSelectedListTasksOnClick(event)')
+      parentDiv.childNodes[1].classList.remove('hide')
+      parentDiv.childNodes[2].classList.remove('hide')
+    }
+  })
+}
 const load = async () => {
   document.getElementById('lists-container').classList.remove('hide')
 
@@ -76,18 +130,23 @@ const load = async () => {
     }
   }
 
-  lists = await fetchDB(reqObj)
-  renderLists(lists)
+  const lists = await fetchDB(reqObj)
+  lists.forEach(list => renderLists(list))
 }
 
 load()
 
 const addNewList = async (reqObj) => {
-  const list = await fetchDB(reqObj)
-  lists.push(list)
+  const newList = await fetchDB(reqObj)
+  renderLists(newList[0])
+  // lists.push(newList[0])
 }
 
-addListInput.addEventListener('keyup', async function (event) {
+const updateList = async (reqObj) => {
+  const updatedList = await fetchDB(reqObj)
+}
+
+addListInput.addEventListener('keyup', function (event) {
   // searchList(event)
 
   if (event.keyCode === 13) {
@@ -114,6 +173,9 @@ addListInput.addEventListener('keyup', async function (event) {
     addListInput.placeholder = ' Search | Add Lists'
     // console.log(addListInput.nextSibling.innerHTML)
     // reset(addListInput.nextElementSibling)
-    renderLists(lists)
   }
 })
+
+const renderTask = () => {
+
+}
--- queries.js
+++ queries.js
@@ -51,10 +51,9 @@ queriesObject.updateList = async (req, res) => {
   try {
     const listId = Number(req.params.id)
     const listName = req.body.listName
-    const result = await pool.query('UPDATE LISTS SET list_name = $1 WHERE list_id = $2', [listName, listId])
-    // console.log(result)
+    const result = await pool.query('UPDATE LISTS SET list_name = $1 WHERE list_id = $2 RETURNING *', [listName, listId])
     if (result.rowCount === 0) return res.status(404).json({ message: `can't find list with id ${listId}` })
-    res.status(200).json({ message: `List modified with ID: ${listId}` })
+    res.status(200).json(result.rows)
   } catch (e) {
     res.status(500).json({ message: `Can't update list of ${req.params.id} id` })
   }
